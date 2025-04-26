0fd422b44c786e344f9eb9810119aa71dd5ee5e4
Haselnussbomber <mail@haselnussbomber.de>
2025-04-26 03:03:55 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Reloaded.Memory.Sigscan;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Usage: FindSig <signature>");
+    return;
+}
+
+var signature = args[0];
+
+Console.WriteLine($"Scanning for sig: {signature}");
+
+foreach (string file in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.exe", SearchOption.AllDirectories))
+{
+    using var scanner = new Scanner(File.ReadAllBytes(file));
+    var result = scanner.FindPattern(signature);
+    if (result.Found)
+        Console.WriteLine($"Found at offset 0x{result.Offset:X} in {file}");
+}
